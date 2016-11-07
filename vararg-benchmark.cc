10803fa6a1b62e38843135b107fef299447941a8
Jacek Galowicz <jacek@galowicz.de>
2016-11-07 09:16:05 +0100

    Add some benchmarks with 12 parameters to see how libfmt scales in comparison with printf

--- vararg-benchmark.cc
+++ vararg-benchmark.cc
@@ -22,6 +22,16 @@ void varargs(benchmark::State& state) {
     test_printf("%d", 42);
 }
 
+void varargs_12(benchmark::State& state) {
+  while (state.KeepRunning())
+    test_printf("%d %d %d %d "
+                "%f %f %f %f "
+                "%x %x %x %x",
+                1,    2,    3,    4,
+                1.0f, 2.0f, 3.0f, 4.0f,
+                0x1,  0x2,  0x3,  0x4);
+}
+
 BENCHMARK(varargs);
 
 void __attribute__((noinline)) test_vprint(const char *f, fmt::format_args) {
@@ -38,6 +48,16 @@ void fmt_variadic(benchmark::State &state) {
     test_print("{}", 42);
 }
 
+void fmt_variadic_12(benchmark::State &state) {
+  while (state.KeepRunning())
+    test_printf("{} {} {} {} "
+                "{} {} {} {} "
+                "{} {} {} {}",
+                1,    2,    3,    4,
+                1.0f, 2.0f, 3.0f, 4.0f,
+                0x1,  0x2,  0x3,  0x4);
+}
+
 BENCHMARK(fmt_variadic);
 
 void test_sprintf(benchmark::State &state) {
@@ -46,11 +66,32 @@ void test_sprintf(benchmark::State &state) {
     std::sprintf(buffer, "%d", 42);
 }
 
+void test_sprintf_12(benchmark::State &state) {
+  char buffer[64];
+  while (state.KeepRunning())
+    std::sprintf(buffer, "%d %d %d %d "
+                         "%f %f %f %f "
+                         "%x %x %x %x",
+                         1,    2,    3,    4,
+                         1.0f, 2.0f, 3.0f, 4.0f,
+                         0x1,  0x2,  0x3,  0x4);
+}
+
 BENCHMARK(test_sprintf);
 
 void test_format(benchmark::State &state) {
   while (state.KeepRunning())
     fmt::format("{}", 42);
+}
+
+void test_format_12(benchmark::State &state) {
+  while (state.KeepRunning())
+      fmt::format("{} {} {} {} "
+                  "{} {} {} {} "
+                  "{} {} {} {}",
+                  1,    2,    3,    4,
+                  1.0f, 2.0f, 3.0f, 4.0f,
+                  0x1,  0x2,  0x3,  0x4);
 }
 
 BENCHMARK(test_format);
@@ -61,6 +102,17 @@ void test_sprintf_pos(benchmark::State &state) {
     std::sprintf(buffer, "%1$d", 42);
 }
 
+void test_sprintf_pos_12(benchmark::State &state) {
+  char buffer[64];
+  while (state.KeepRunning())
+    std::sprintf(buffer, "%12$d %11$d %10$d %9$d "
+                         "%8$f %7$f %6$f %5$f "
+                         "%4$x %3$x %2$x %1$x",
+                         0x1,  0x2,  0x3,  0x4,
+                         1.0f, 2.0f, 3.0f, 4.0f,
+                         1,    2,    3,    4);
+}
+
 BENCHMARK(test_sprintf_pos);
 
 void test_format_pos(benchmark::State &state) {
@@ -68,6 +120,23 @@ void test_format_pos(benchmark::State &state) {
     fmt::format("{0}", 42);
 }
 
+void test_format_pos_12(benchmark::State &state) {
+  while (state.KeepRunning())
+      fmt::format("{11} {10} {9} {8} "
+                  "{7} {6} {5} {4} "
+                  "{3} {2} {1} {0}",
+                  1,    2,    3,    4,
+                  1.0f, 2.0f, 3.0f, 4.0f,
+                  0x1,  0x2,  0x3,  0x4);
+}
+
 BENCHMARK(test_format_pos);
+
+BENCHMARK(varargs_12);
+BENCHMARK(fmt_variadic_12);
+BENCHMARK(test_sprintf_12);
+BENCHMARK(test_format_12);
+BENCHMARK(test_sprintf_pos_12);
+BENCHMARK(test_format_pos_12);
 
 BENCHMARK_MAIN();
