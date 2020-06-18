b3c4a9453075e3f6adc0cf207571fa65e191196a
Victor Zverovich <viz@fb.com>
2020-06-17 20:47:54 -0700

    Add hopman

--- src/int-benchmark.cc
+++ src/int-benchmark.cc
@@ -22,6 +22,76 @@
 
 #include "itostr.cc"
 
+namespace hopman_fast
+{
+   struct itostr_helper
+   {
+      static unsigned out[10000];
+
+      itostr_helper()
+      {
+         for (int i = 0; i < 10000; i++)
+         {
+            unsigned v = i;
+            char * o = (char*)(out + i);
+            o[3] = v % 10 + '0';
+            o[2] = (v % 100) / 10 + '0';
+            o[1] = static_cast<char>((v % 1000) / 100) + '0';
+            o[0] = static_cast<char>((v % 10000) / 1000);
+                 if (o[0])        o[0] |= 0x30;
+            else if (o[1] != '0') o[0] |= 0x20;
+            else if (o[2] != '0') o[0] |= 0x10;
+            else                  o[0] |= 0x00;
+         }
+      }
+   };
+
+   unsigned itostr_helper::out[10000];
+
+   itostr_helper hlp_init;
+
+   template <typename T>
+   void itostr(T o, std::string& out)
+   {
+      typedef itostr_helper hlp;
+
+      unsigned blocks[3], *b = blocks + 2;
+      blocks[0] = o < 0 ? ~o + 1 : o;
+      blocks[2] = blocks[0] % 10000; blocks[0] /= 10000;
+      blocks[2] = hlp::out[blocks[2]];
+
+      if (blocks[0])
+      {
+         blocks[1]  = blocks[0] % 10000; blocks[0] /= 10000;
+         blocks[1]  = hlp::out[blocks[1]];
+         blocks[2] |= 0x30303030;
+         b--;
+      }
+
+      if (blocks[0])
+      {
+         blocks[0]  = hlp::out[blocks[0] % 10000];
+         blocks[1] |= 0x30303030;
+         b--;
+      }
+
+      char* f = ((char*)b);
+      f += 3 - (*f >> 4);
+
+      char* str = (char*)blocks;
+      if (o < 0) *--f = '-';
+      out.assign(f, (str + 12) - f);
+   }
+
+   template <typename T>
+   std::string itostr(T o)
+   {
+      std::string result;
+      itostr(o,result);
+      return result;
+   }
+}
+
 // Integer to string converter by Alf P. Steinbach modified to return a pointer
 // past the end of the output to avoid calling strlen.
 namespace cppx {
@@ -159,6 +229,17 @@ struct DigestChecker {
     digest += compute_digest(s);
   }
 };
+
+void hopman(benchmark::State& state) {
+  auto dc = DigestChecker(state);
+  for (auto s : state) {
+    for (auto value : data) {
+      auto s = hopman_fast::itostr(value);
+      dc.add(s);
+    }
+  }
+}
+BENCHMARK(hopman);
 
 void sprintf(benchmark::State& state) {
   auto dc = DigestChecker(state);
