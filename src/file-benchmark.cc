b9279157733edade4a666e41fee53e4fc888879e
Victor Zverovich <viz@fb.com>
2020-08-04 07:41:36 -0700

    Update file-benchmark

--- src/file-benchmark.cc
+++ src/file-benchmark.cc
@@ -5,11 +5,12 @@
 #include <fstream>
 #include <stdio.h>
 
-const char* test_str = "Hello, world!";
-int num_iters = 1'000'000;
+auto test_str = "[testdata]";
+auto num_iters = 1'000'000;
 
 void fprintf(benchmark::State& state) {
-  FILE* f = fopen("1.txt", "wb");
+  auto path = "/tmp/fprintf-test.txt";
+  auto f = fopen(path, "wb");
   for (auto s : state) {
     for (int i = 0; i < num_iters; ++i)
       fprintf(f, "%s\n", test_str);
@@ -19,7 +20,9 @@ void fprintf(benchmark::State& state) {
 BENCHMARK(fprintf);
 
 void std_ofstream(benchmark::State& state) {
-  std::ofstream os("2.txt", std::ios::binary);
+  auto path = "/tmp/ofstream-test.txt";
+  std::remove(path);
+  auto os = std::ofstream(path, std::ios::binary);
   for (auto s : state) {
     for (int i = 0; i < num_iters; ++i)
       os << test_str << '\n';
@@ -28,21 +31,36 @@ void std_ofstream(benchmark::State& state) {
 BENCHMARK(std_ofstream);
 
 void fmt_print_compile(benchmark::State& state) {
-  auto f = fmt::output_file("3.txt");
+  auto path = "/tmp/fmt-compile-test.txt";
+  std::remove(path);
+  auto f = fmt::output_file(path, fmt::buffer_size=state.range(0));
   for (auto s : state) {
     for (int i = 0; i < num_iters; ++i)
       f.print(FMT_COMPILE("{}\n"), test_str);
   }
 }
-BENCHMARK(fmt_print_compile);
+BENCHMARK(fmt_print_compile)->RangeMultiplier(2)->Range(BUFSIZ, 1 << 20);
 
 void fmt_print_runtime(benchmark::State& state) {
-  auto f = fmt::output_file("4.txt");
+  auto path = "/tmp/fmt-runtime-test.txt";
+  std::remove(path);
+  auto f = fmt::output_file(path, fmt::buffer_size=state.range(0));
   for (auto s : state) {
     for (int i = 0; i < num_iters; ++i)
       f.print("{}\n", test_str);
   }
 }
-BENCHMARK(fmt_print_runtime);
+BENCHMARK(fmt_print_runtime)->RangeMultiplier(2)->Range(BUFSIZ, 1 << 20);
+
+void fmt_print_compile_default(benchmark::State& state) {
+  auto path = "/tmp/fmt-compile-default-test.txt";
+  std::remove(path);
+  auto f = fmt::output_file(path);
+  for (auto s : state) {
+    for (int i = 0; i < num_iters; ++i)
+      f.print(FMT_COMPILE("{}\n"), test_str);
+  }
+}
+BENCHMARK(fmt_print_compile_default);
 
 BENCHMARK_MAIN();
