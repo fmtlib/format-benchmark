cf7e8ad45745142b3de56cca92af1977afd5f932
Victor Zverovich <viz@fb.com>
2020-08-05 07:28:26 -0700

    Rename all the things

--- src/file-benchmark.cc
+++ src/file-benchmark.cc
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <stdio.h>
 
-auto test_str = "test data";
+auto test_data = "test data";
 auto num_iters = 1'000'000;
 
 const char* removed(benchmark::State& state, const char* path) {
@@ -19,7 +19,7 @@ void fprintf(benchmark::State& state) {
   for (auto s : state) {
     auto f = fopen(removed(state, "/tmp/fprintf-test"), "wb");
     for (int i = 0; i < num_iters; ++i)
-      fprintf(f, "%s\n", test_str);
+      fprintf(f, "%d\n", test_data);
     fclose(f);
   }
 }
@@ -30,7 +30,7 @@ void std_ofstream(benchmark::State& state) {
     auto os = std::ofstream(
       removed(state, "/tmp/ofstream-test"), std::ios::binary);
     for (int i = 0; i < num_iters; ++i)
-      os << test_str << '\n';
+      os << test_data << '\n';
   }
 }
 BENCHMARK(std_ofstream);
@@ -40,7 +40,7 @@ void fmt_print_compile(benchmark::State& state) {
     auto f = fmt::output_file(removed(state, "/tmp/fmt-compile-test"),
                               fmt::buffer_size=state.range(0));
     for (int i = 0; i < num_iters; ++i)
-      f.print(FMT_COMPILE("{}\n"), test_str);
+      f.print(FMT_COMPILE("{}\n"), test_data);
   }
 }
 BENCHMARK(fmt_print_compile)->RangeMultiplier(2)->Range(BUFSIZ, 1 << 20);
@@ -50,7 +50,7 @@ void fmt_print_runtime(benchmark::State& state) {
     auto f = fmt::output_file(removed(state, "/tmp/fmt-runtime-test"),
                               fmt::buffer_size=state.range(0));
     for (int i = 0; i < num_iters; ++i)
-      f.print("{}\n", test_str);
+      f.print("{}\n", test_data);
   }
 }
 BENCHMARK(fmt_print_runtime)->RangeMultiplier(2)->Range(BUFSIZ, 1 << 20);
@@ -60,7 +60,7 @@ void fmt_print_compile_default(benchmark::State& state) {
     auto f = fmt::output_file(
       removed(state, "/tmp/fmt-compile-default-test"));
     for (int i = 0; i < num_iters; ++i)
-      f.print(FMT_COMPILE("{}\n"), test_str);
+      f.print(FMT_COMPILE("{}\n"), test_data);
   }
 }
 BENCHMARK(fmt_print_compile_default);
